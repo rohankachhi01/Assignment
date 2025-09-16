f4480db9478a37015965bb9640dd116ffec6374e
rohan kachhi <rohangaming192@gmail.com>
2025-09-16 17:37:00 +0530

    Assignment 1
    I hopeful you fulfill your requirement I'm trying to best to give

---
+++ Assignment 1.py
@@ -0,0 +1,25 @@
+#Assignment 1
+
+
+#  Write a Python program that does the following:
+# 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+a = int(input("enter the first number :"))
+b = int(input("enter the second number :"))
+sum = a + b #addtion
+sub = a - b #Subtraction
+mul = a * b #Multiplication
+div = a / b #Division
+print(sum,sub,mul,div)
+
+# Write a Python program that:
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+
+a = (input("enter the first name :"))
+b = (input("enter the last name :"))
+print("thank you enter you name Mr/Ms",a,b,"welcome you")
+
+git config --global user.name "rohankachhi01"
+git config --global user.email "rohankachhi88@gmail.con"
