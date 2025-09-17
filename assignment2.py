b25e888678e8e4e96b79aa3437d7c0404289406d
rohan kachhi <rohangaming192@gmail.com>
2025-09-17 18:07:45 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,20 @@
+# ASSIGNMENT 2:
+# Module 3: Control Structures in Python
+#date: 2023-09-20   
+
+# "Task 1: Check if a Number is Even or Odd"
+
+a = int(input("Enter a number: "))
+if a % 2 == 0:
+    print("task1 The number", a, "is Even")
+else:
+    print("The number", a, "is Odd")
+
+
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+a = 0
+for b in range(1, 51):
+    a += b
+print("task2 The sum of all additon for 1 to 50 is:", a) 
+
